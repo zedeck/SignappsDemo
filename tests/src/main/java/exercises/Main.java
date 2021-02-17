f38c47eafd59f51b08e31504ab113d9cff611ff2
godwill <godwillm@computicket.com>
2021-02-17 17:52:41 +0200

    bare maven project

---
+++ tests/src/main/java/exercises/Main.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test done by Godwill.");
+	}
+
+}
